1d1b7b9480a059c25a9bfb9c2941a6d15da18816
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-13 21:15:46 +0530

    grappling hook

--- 3D Games/Minecraft.py
+++ 3D Games/Minecraft.py
@@ -44,47 +44,47 @@ class Voxel(Button):
             color=default_color,
         )
 
-for y in range(0,101,50):
-    for z in range(12):
-        for x in range(12):
+for y in range(0,51,25):
+    for z in range(10):
+        for x in range(10):
             voxel = Voxel(position=(x,y,z))
 
 def input(key):
-    global c
-    c+=1
+    global c, player
+
+    hit_info = raycast(camera.world_position, camera.forward, distance=100)
+    # print(player.x, hit_info.entity.position.x)
+
+    if key == 'right mouse down': # grappling hook (to make smooth)
+        player.x = hit_info.entity.position.x
+        player.y = hit_info.entity.position.y
+        player.z = hit_info.entity.position.z
+
+    if key == 'c':
+        c+=1
 
     if key == 'left mouse down':
-        hit_info = raycast(camera.world_position, camera.forward, distance=100)
         if hit_info.hit:
             Voxel(position=hit_info.entity.position + hit_info.normal, 
-                  texture='brick',
-                  default_color=color.orange,
-                #   texture=opt_texture[c%len(opt_texture)],
-                #   default_color=color.random_color(),
-                  )
-    if key == 'right mouse down' and mouse.hovered_entity:
+                #   texture='brick',
+                #   default_color=color.orange,
+                    texture=opt_texture[c%len(opt_texture)],
+                    default_color=color.random_color(),
+                    )
+            
+    if key == 'e' and mouse.hovered_entity:
         destroy(mouse.hovered_entity)
 
 
 window.fullscreen = 1
-player = FirstPersonController(gravity=.6)
+player = FirstPersonController(gravity=.2)
 
 # https://stackoverflow.com/a/75692459/11493297
 def update():
-    print('Player is at Height = ', player.y)
+    # print('Player is at Height = ', player.y)
 
-    if player.y < -10:
-        player.y = 110   # No infinite falling.
-
-# player = FirstPersonController(
-                    # model='sphere', 
-                    # texture='white_cube',
-                    # color=color.blue,
-                    # origin = (0, -1, -2),
-                    # )
-
-# skybox_image = load_texture("sunset.jpg")
-# sky = Sky(texture=skybox_image)
+    if player.y < -5:
+        player.y = 30 
 
 Sky()
 app.run()
